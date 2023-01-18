91b600a68579dca66708e59d0441968181ec253e
Mauricio Lorenzo Guerrero González <bazzvkngo@gmail.com>
2023-01-18 00:26:47 -0300

    Primer Commit

---
+++ src/main/java/com/actividaduno/Main.java
@@ -0,0 +1,13 @@
+package com.actividaduno;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
